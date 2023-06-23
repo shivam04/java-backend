0344758b16fe534964aa76110cc5ff5ba440e45c
Shivam Sinha <shvmsnha@amazon.com>
2023-06-23 20:12:39 +0530

    add http code

--- src/main/java/org/backend/Main.java
+++ src/main/java/org/backend/Main.java
@@ -1,7 +1,25 @@
 package org.backend;
 
+import org.backend.greetings.Greetings;
+import org.backend.http.Client;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Argument passed is: " + Arrays.toString(args));
+        switch (args[0]) {
+            case "greet":
+                Greetings greet = new Greetings();
+                System.out.println(greet.greet());
+                break;
+            case "http":
+                Client client = new Client();
+                String response = client.get("https://square.github.io/okhttp/");
+                System.out.println(response);
+                break;
+            default:
+                System.out.println("Invalid argument passed");
+        }
     }
 }
